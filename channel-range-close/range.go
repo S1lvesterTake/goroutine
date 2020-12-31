6a230c6149a755c0016554f62853e81018e98104
silvester <silvetake@gmail.com>
2020-12-31 17:16:14 +0700

    add channel - range - close

---
+++ channel-range-close/range.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main() {
+	channel := make(chan int, 5)
+	channel <- 1
+	channel <- 2
+	channel <- 3
+	close(channel)
+
+	for element := range channel {
+		fmt.Println(element)
+	}
+
+	channel <- 3
+}
