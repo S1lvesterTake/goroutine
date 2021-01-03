c5fd64ccf66d795c3679110d2ad4218e90a2b679
silvester <silvetake@gmail.com>
2021-01-03 12:35:44 +0700

    add channel timeout

---
+++ channel-timeout/timeout.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"runtime"
+	"time"
+)
+
+func main() {
+	runtime.GOMAXPROCS(2)
+
+	var messages = make(chan int)
+
+	go sendData(messages)
+	retreiveData(messages)
+}
+
+func sendData(ch chan<- int) {
+	for i := 0; true; i++ {
+		ch <- i
+		time.Sleep(time.Duration(rand.Intn(10)) * time.Second)
+	}
+}
+
+func retreiveData(ch <-chan int) {
+loop:
+	for {
+		select {
+		case data := <-ch:
+			fmt.Print(`receive data "`, data, `"`, "\n")
+		case <-time.After(time.Second * 5):
+			fmt.Println("timeout. no activities under 5 seconds")
+			break loop
+		}
+	}
+}
